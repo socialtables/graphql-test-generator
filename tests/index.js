a3cfed9f22b2cfe2c58519f83953876800ac8c37
Katherine Thompson <katherine.thompson@socialtables.com>
2017-09-21 14:48:22 -0400

    Add tests for failing schemas.

--- tests/index.js
+++ tests/index.js
@@ -2,6 +2,11 @@ const { graphqlTestGenerator, gqlTestExtraction } = require('../index');
 const path = require('path');
 
 const queryPath = path.join(__dirname, '../example');
-graphqlTestGenerator({ entry: queryPath, output: path.join(__dirname, '../generatedTests-graphqlTestGenerator'), schemaLocation: '../tests/schema', overwriteFiles: true });
+graphqlTestGenerator({ entry: queryPath, output: path.join(__dirname, '../generatedTests/graphqlTestGenerator'), schemaLocation: '../../tests/schema', overwriteFiles: true });
 
-gqlTestExtraction({ entry: queryPath, output: path.join(__dirname, '../generatedTests-gqlTestExtraction'), schemaLocation: '../tests/schema', overwriteFiles: true });
+gqlTestExtraction({ entry: queryPath, output: path.join(__dirname, '../generatedTests/gqlTestExtraction'), schemaLocation: '../../tests/schema', overwriteFiles: true });
+
+const failQueryPath = path.join(__dirname, '../exampleWillFail');
+graphqlTestGenerator({ entry: failQueryPath, output: path.join(__dirname, '../generatedTests/graphqlTestGeneratorWillFail'), schemaLocation: '../../tests/schema', overwriteFiles: true, importLocation: './tests/test-for-failure' });
+
+gqlTestExtraction({ entry: failQueryPath, output: path.join(__dirname, '../generatedTests/gqlTestExtractionWillFail'), schemaLocation: '../../tests/schema', overwriteFiles: true, importLocation: './tests/test-for-failure' });
